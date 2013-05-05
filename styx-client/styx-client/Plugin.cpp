658bd9217d7255b580442c87339cc6a135028467
ForNeVeR <neverthness@gmail.com>
2013-05-05 21:40:42 +0700

    Clear coding style, adapt some code.

--- styx-client/styx-client/Plugin.cpp
+++ styx-client/styx-client/Plugin.cpp
@@ -6,7 +6,11 @@
 #include <m_clist.h>
 #include <m_skin.h>
 
-HINSTANCE hInst = NULL;
+const auto EnableServiceName = "Styx/EnableCommand";
+
+HINSTANCE libraryInstance = nullptr;
+
+// Do not remove - part of API.
 int hLangpack;
 
 PLUGININFOEX pluginInfo = {
@@ -22,43 +26,46 @@ PLUGININFOEX pluginInfo = {
 	{ 0xf38aa380, 0x2267, 0x4ff7, { 0x9b, 0x19, 0xae, 0x96, 0x6b, 0xac, 0x49, 0xd8 } } // {F38AA380-2267-4FF7-9B19-AE966BAC49D8}
 };
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved)
 {
-	hInst = hinstDLL;
+	libraryInstance = instance;
 	return TRUE;
 }
 
-INT_PTR PluginMenuCommand(WPARAM wParam, LPARAM lParam)
+INT_PTR EnableCommand(WPARAM wParam, LPARAM lParam)
 {
-	MessageBox(NULL, TranslateT("Just groovy, baby!"), TranslateT("Plugin-o-rama"), MB_OK);
+	// TODO: Enable synchronization if not enabled.
 	return 0;
 }
 
-extern "C" __declspec(dllexport) PLUGININFOEX* MirandaPluginInfoEx(DWORD mirandaVersion)
+extern "C" __declspec(dllexport) PLUGININFOEX *MirandaPluginInfoEx(DWORD mirandaVersion)
 {
 	return &pluginInfo;
 }
 
-extern "C" __declspec(dllexport) const MUUID interfaces[] = {MIID_TESTPLUGIN, MIID_LAST};
+extern "C" __declspec(dllexport) const MUUID interfaces[] = { MIID_LAST };
 
 extern "C" __declspec(dllexport) int Load()
 {
 	mir_getLP(&pluginInfo);
 
-	CLISTMENUITEM mi = {0};
+	CreateServiceFunction(EnableServiceName, &EnableCommand);
 
-	CreateServiceFunction("TestPlug/MenuCommand", PluginMenuCommand);
+	auto mi = CLISTMENUITEM();
 	mi.cbSize = sizeof(mi);
 	mi.position = -0x7FFFFFFF;
-	mi.flags = CMIF_TCHAR;
+	mi.flags = CMIF_UNICODE;
 	mi.hIcon = LoadSkinnedIcon(SKINICON_OTHER_MIRANDA);
-	mi.ptszName = LPGENT("&Test Plugin...");
-	mi.pszService = "TestPlug/MenuCommand";
+	mi.ptszName = LPGENT("&Enable Styx");
+	mi.pszService = const_cast<char*>(EnableServiceName);
+
 	Menu_AddMainMenuItem(&mi);
+
 	return 0;
 }
 
 extern "C" __declspec(dllexport) int Unload(void)
 {
+	// TODO: Disable synchronization if enabled.
 	return 0;
 }
