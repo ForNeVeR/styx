dc2c48dc025c62836babc8667432d52172207237
ForNeVeR <neverthness@gmail.com>
2013-09-08 14:55:37 +0700

    Start implementing server-side ChunkHash processing.

--- styx-server/src/main/scala/ru/org/codingteam/styx/server/ClientActor.scala
+++ styx-server/src/main/scala/ru/org/codingteam/styx/server/ClientActor.scala
@@ -4,19 +4,22 @@ import akka.actor.{Actor, ActorLogging}
 import ru.org.codingteam.styx.LoginDef.Login
 import ru.org.codingteam.styx.LoginResultDef.LoginResult
 import com.google.protobuf.Message
+import ru.org.codingteam.styx.ChunkHashDef.ChunkHash
 
 class ClientActor extends Actor with ActorLogging {
 	private var state: SynchronizerState = NotConnected
 
 	override def receive = {
 		case loginRequest: Login =>
-			processLoginRequest(loginRequest)
+			sender ! processLoginRequest(loginRequest)
+		case chunkHash: ChunkHash =>
+			sender ! processChunkHash(chunkHash)
 		case UnknownMessage =>
 			log.info("Got unknown message")
 	}
 
-	private def processLoginRequest(request: Login) {
-		val result: Message = state match {
+	private def processLoginRequest(request: Login) = {
+		state match {
 			case NotConnected =>
 				// TODO: Check user credientials
 				val status = request.getUsername == "user" && request.getPassword == "password"
@@ -26,10 +29,19 @@ class ClientActor extends Actor with ActorLogging {
 				}
 				message
 			case _ =>
-				val message = ru.org.codingteam.styx.ErrorDef.Error.newBuilder().setReason("Already logged in").build()
-				message
+				createError("Already logged in")
 		}
-
-		sender ! result
 	}
+
+	private def processChunkHash(request: ChunkHash) = {
+		state match {
+			case Hashing =>
+				// TODO: Check hash with the StorageActor.
+			case _ =>
+				createError("Cannot process hash when not in Hashing state")
+		}
+	}
+
+	private def createError(reason: String) =
+		ru.org.codingteam.styx.ErrorDef.Error.newBuilder().setReason(reason).build()
 }
